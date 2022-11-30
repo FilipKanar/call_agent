19bce63d4ff6bf5aeeaa41819e2f2ca633aad699
Borsuk <antena309@gmail.com>
2022-11-30 14:21:37 +0100

    Bloc Fix

--- lib/custom_providers.dart
+++ lib/custom_providers.dart
@@ -1,12 +1,21 @@
+import 'package:call_agent/bloc/authentication/authentication_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'data/api/authentication/firebase_authentication_service.dart';
 import 'data/api/authentication_service.dart';
 
 class CustomProviders {
-  static final myRepositories = [
+  static final repositories = [
     RepositoryProvider<AuthenticationService>(
       create: (context) => FirebaseAuthenticationService(),
     ),
+  ];
+  static final blocs = [
+    BlocProvider(
+      create: (context) => AuthenticationBloc(
+        authenticationService:
+            RepositoryProvider.of<AuthenticationService>(context),
+      ),
+    )
   ];
 }
